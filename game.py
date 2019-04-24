82bd2acaf8fe3258100652458b5c1822978fdf0d
Yuchunw <43788013+Yuchunw@users.noreply.github.com>
2019-04-23 22:15:01 -0400

    Update game.py

--- game.py
+++ game.py
@@ -4,9 +4,9 @@
 
 # -*- coding: utf-8 -*-
 
-import pygame, random
+import pygame, random, time, sys, itertools, os
+from setup import *
 
-gems = {0: 'red', 1: 'green', 2: 'blue' , 3:'yellow'}
 
 class cell:
 	# Cell on the board 
@@ -14,13 +14,63 @@ class cell:
 	# Add properties of cell here!
 	def _init_(self, image):
 		self.offset = 0.0
-		self.image = image
+		self.cells = monsters
 
 
 class board:
 	# A rectangular board of cells
 	# Properties: width, height, size and etc...
-	def _init_(self):
+	def _init_(self, width, height):
+		self.width = boardwidth
+		self.height = boardheight
+		self.size = boardheight * boardwidth
+		self.board = [cell(self.blank) for _ in range(self.size)]
+		self.matches = []
+		self.refill = []
+
+	def pos(self, i, j):
+		assert(0 <= i < self.width)
+		assert(0 <= j < self.height)
+		return j * self.width + i
+
+	def findmatch(self):
+		def lines():
+			for j in range (self.height):
+				yield range(j * self.width, (j + 1) * self.width)
+			for i in range (self.width):
+				yield range(i, self.size, self.width)
+		def key(i):
+			return self.board[i].image
+		def matches():
+			for lin in lines():
+				for _, group in itertools.groupby(line, key):
+					match = list(group)
+					if len(match) >= matchmin:
+						yield match
+		return list(matches())
+
+	def updatematch(self, image):
+		for match in self.matches:
+			for position in match:
+				self.board[position].image = image
+
+	def refill(self):
+		for i in range(self.width):
+			target = self.size - i -1
+			for pos in range(target, -1, -self.width):
+				if self.board[pos].image != self.blank:
+					c = self.board[target]
+					c.image = self.board[pos].image
+					c.offset = (target - pos) // self.width
+					target -= self.width
+					yield c
+			offset = 1 + (target - pos) // self.width
+			for pos in range(target, -1, -self.width):
+				c = self.board[pos]
+				c.image = random.choice(self.cells)
+				c.offset = offset
+				yield c
+
 
 
 
