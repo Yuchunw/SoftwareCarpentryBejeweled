06b592840440140fb6a9252338324de253b6a1d2
akarinnn <47227434+akarinnn@users.noreply.github.com>
2019-04-29 21:06:00 -0400

    Update littlemonsters.py

--- ALLUNEED/littlemonsters.py
+++ ALLUNEED/littlemonsters.py
@@ -12,6 +12,9 @@ import os
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+green = (173, 186, 70)
+lightgreen = (243, 246, 221)
+blue = (128, 193, 195)
 
 monster_width = 50               # Width of each shape (pixels).
 monster_height = 50              # Height of each shape (pixels).
@@ -223,7 +226,7 @@ class Game(object):
         self.clock = pygame.time.Clock()
         self.display = pygame.display.set_mode((disp_width, disp_height))
         self.board = Board(game_columns, game_rows)
-        self.font = pygame.font.Font(None, font_size)
+        self.font = pygame.font.Font('CantikaHandwriting.otf', font_size)
 
     def start(self):
         """
@@ -303,12 +306,12 @@ class Game(object):
     def draw_time(self):
         s = int(self.swap_time)
         text = self.font.render('{}:{:02}'.format(s / 60, s % 60),
-                                True, BLACK)
+                                True, blue)
         self.display.blit(text, (400, 115))
 
     def draw_score(self):
     	total_score = self.score + self.board.score
-        text = self.font.render('{}'.format(total_score), True, BLACK)
+        text = self.font.render('{}'.format(total_score), True, green)
         self.display.blit(text, (135, 115))
 
     def draw_cursor(self):
